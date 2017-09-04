84e0bf98393035d4f31ac30a7a7d577f1441116a
Soumya <SSundarr@uk.ibm.com>
2017-09-04 14:14:32 +0100

    Re-work
    Revisit export program fixing shell permission access.

--- ExportNls.js
+++ ExportNls.js
@@ -3,45 +3,46 @@ var find = require('find');
 const path = require('path')
 var shell = require('shelljs')
 
-shell.mkdir('-p', "output")
+var GitHome = path.join(`${shell.pwd()}`,'../')
 
-shell.cd('../')
+var currentDir = path.join(`${shell.pwd()}`)
 
-var GitHome = shell.pwd()
-//shell.echo(GitHome)
+var exportPath = path.join(`${GitHome}`,"translation", "output")
 
-var exportPath = path.join(`${GitHome}`,"ExportScript", "output")
+//Check if directory is empty or delete folders
+if(shell.test('-e', exportPath)) {
+  shell.rm('-r', exportPath)
+}
+console.log(shell.test('-e', exportPath))
+
+shell.mkdir('output')
 
 shell.echo("Nls folders will be exported in: " + exportPath)
 
-// Check if directory is empty or delete folders
-if(fs.existsSync(exportPath)) {
-    fs.removeSync(exportPath)
-}
 
-//console.log(fs.existsSync(exportPath))
+var dashboard = {key : path.join(`${GitHome}`, "ui-dashboard","src", "angular", "nls") , value : path.join("ui-dashboard","src", "angular", "nls")}
 
-var dashboard = path.join("ui-dashboard","src", "angular", "nls")
+var homepage = {key: path.join(`${GitHome}`, "ui-homepage","src", "nls"), value: path.join("ui-homepage","src", "nls")}
 
-var homepage = path.join("ui-homepage","src", "nls")
+var quickstart = {key: path.join(`${GitHome}`, "ui-quickstart", "src", "nls"), value: path.join("ui-quickstart", "src", "nls")}
 
-var quickstart = path.join("ui-quickstart", "src", "nls")
-
-var common = path.join("iotcloud.ui.common","nls")
+var common = {key: path.join(`${GitHome}`, "iotcloud.ui.common","nls"), value: path.join("iotcloud.ui.common","nls")}
 
 var re = new RegExp(/\-en.json$|\-en.ai$/);
 
 var tranversePathArray = [dashboard, homepage, quickstart, common]
 
-tranversePathArray.forEach((tranversePath) => {
-  find.file(re, tranversePath, function(files) {
-    //console.log(files);
-    files.forEach((file) => {
-      var outputFile = path.join(`${exportPath}`, `${file}`)
-      //console.log(outputFile)
-      fs.ensureFileSync(outputFile)
-    });
-  })
+tranversePathArray.map((tranversePath) => {
+  var outputFolder = path.join(`${exportPath}`, `${tranversePath.value}`)
+  shell.mkdir('-p', outputFolder)
+  shell.cd(tranversePath.key)
+  var files = shell.find('.').filter(function(file) { return file.match(re) });
+  files.forEach((file) => {
+    var outputFile = path.join(`${outputFolder}`, `${file}`)
+    //console.log(outputFile)
+    shell.cd(outputFolder)
+    shell.cp(path.join(`${tranversePath.key}`,file), outputFolder)
+  });
 });
 
 shell.echo("Folders were exported")
